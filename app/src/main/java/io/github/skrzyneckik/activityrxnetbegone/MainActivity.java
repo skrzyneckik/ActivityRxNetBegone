07cb28e43266dd03f3f2ab769c22c30b84cf234d
Krzysztof Skrzynecki <skrzynecki.krzysztof@gmail.com>
2017-07-31 22:06:08 +0100

    Registered receiver in MainActivity to handle result intents send from NetworkCallService

--- app/src/main/java/io/github/skrzyneckik/activityrxnetbegone/MainActivity.java
+++ app/src/main/java/io/github/skrzyneckik/activityrxnetbegone/MainActivity.java
@@ -1,13 +1,58 @@
 package io.github.skrzyneckik.activityrxnetbegone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static io.github.skrzyneckik.activityrxnetbegone.NetworkCallService.LONG_RUNNING_TASK_RESULT;
 
 public class MainActivity extends AppCompatActivity {
+
+    BroadcastReceiver receiver;
+
+    AtomicInteger receivedResultCount = new AtomicInteger(0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        startService(new Intent(this, NetworkCallService.class));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                receivedResultCount.getAndIncrement();
+                Toast.makeText(MainActivity.this,
+                    String.format("received intent: %d", receivedResultCount.intValue()),
+                    Toast.LENGTH_LONG).show();
+            }
+        };
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+            receiver,
+            new IntentFilter(LONG_RUNNING_TASK_RESULT));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(receiver);
     }
 }
